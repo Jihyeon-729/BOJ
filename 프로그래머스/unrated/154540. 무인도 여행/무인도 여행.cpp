52acc322bd3e4d9725ecbaacdb43d5105bd85f43
Jihyeon-729 <80992891+Jihyeon-729@users.noreply.github.com>
2023-09-28 00:55:42 +0900

    [unrated] Title: 무인도 여행, Time: 0.11 ms, Memory: 4.21 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/154540. 무인도 여행/무인도 여행.cpp
@@ -0,0 +1,66 @@
+#include <string>
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int dx[4] = {1, 0, -1, 0};
+int dy[4] = {0, -1, 0, 1};
+bool visited[102][102];
+
+int DFS(int cx, int cy, int width, vector<string> maps);
+
+vector<int> solution(vector<string> maps) {
+    int nx, ny, width, days;
+    vector<int> answer;
+
+    // DFS
+    width = maps[0].size();
+    for(int i=0; i<maps.size(); i++) {
+        for(int j=0; j<maps[0].size(); j++) {
+            // cout << "(i,j)=(" << i << "," << j << ")\n";
+            // elem is part of unknown island
+            if(maps[i][j] != 'X' && !visited[i][j]){
+                days = DFS(i, j, width, maps);
+                //cout << days << " ";
+                answer.push_back(days);
+            }
+        }
+    }
+
+    sort(answer.begin(), answer.end());
+    if(answer.empty())
+        answer.push_back(-1);
+
+    return answer;
+}
+
+
+// 앞으로 DFS, BFS format은 cy, cx로 다 통일하자
+int DFS(int cy, int cx, int width, vector<string> maps) {
+    int nx, ny, days;
+    days = maps[cy][cx] - '0';
+    visited[cy][cx] = true;
+
+    /* for debugging
+    cout << "cy,cx in DFS = (" << cy << "," << cx << ")\n";
+    for(int i=0; i<maps.size(); i++) {
+            for(int j=0; j<maps[0].size(); j++) {
+                    cout << visited[i][j] << " ";
+            }
+            cout << "\n";
+    }
+    */
+
+
+    for(int i=0; i<4; i++) {
+        nx = cx + dx[i]; ny = cy + dy[i];
+        if(0<=nx&&nx<maps[0].size() && 0<=ny&&ny<maps.size()) { // in map
+            if(!visited[ny][nx] && maps[ny][nx] != 'X')
+                days += DFS(ny, nx, width, maps);
+        }
+    }
+
+    return days;
+}
