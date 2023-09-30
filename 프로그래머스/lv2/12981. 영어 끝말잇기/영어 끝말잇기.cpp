e7a353e72fa2fc6360a3dd48a72afe602b36b6fc
Jihyeon-729 <80992891+Jihyeon-729@users.noreply.github.com>
2023-10-01 00:07:27 +0900

    [level 2] Title: 영어 끝말잇기, Time: 0.03 ms, Memory: 4 MB -BaekjoonHub

--- 프로그래머스/lv2/12981. 영어 끝말잇기/영어 끝말잇기.cpp
+++ 프로그래머스/lv2/12981. 영어 끝말잇기/영어 끝말잇기.cpp
@@ -1,16 +1,24 @@
 #include <string>
 #include <vector>
-#include <map>
- 
+#include <cmath>
+#include <iostream>
+
 using namespace std;
- 
+
 vector<int> solution(int n, vector<string> words) {
-    map<string, int> word;
-    word[words[0]]++;
-    for(int i=1; i<words.size(); i++){
-        if(word[words[i]] || words[i].front() != words[i-1].back())
-            return {(i%n)+1,(i/n)+1};
-        word[words[i]]++;
+    bool fail = false;
+    vector<int> answer;
+    
+    for(int i=1; i<words.size(); i++) {
+        if (words[i][0] != words[i-1][words[i-1].size()-1]) {
+            return {(i%n)+1, (i/n)+1};
+        }
+        for(int j=0; j<i; j++) {
+            if(words[i] == words[j]) {
+                return {(i%n)+1, (i/n)+1};
+            }
+        }
     }
+    
     return {0, 0};
 }
